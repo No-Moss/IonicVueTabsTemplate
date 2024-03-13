78ae6ccc35cb2fecc9f2fc072526e47070daeda9
Amina Mustafa <aminamustafa@Aminas-MBP.modem>
2024-03-13 11:47:08 +1100

    feat: adds test swiper

---
+++ src/components/SwiperContainer.vue
@@ -0,0 +1,32 @@
+<template>
+  <swiper class="testing" :slides-per-view="1" :loop="false">
+    <swiper-slide class="testSlide1">Slide 1</swiper-slide>
+    <swiper-slide class="testSlide2">Slide 2</swiper-slide>
+    <swiper-slide class="testSlide3">Slide 3</swiper-slide>
+  </swiper>
+</template>
+<script setup lang="ts">
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/css";
+</script>
+
+<style scoped>
+.testing {
+  background: white;
+  height: 100px;
+  width: 100px;
+  top: 100px;
+  padding-top: 40px;
+  text-align: center;
+  margin: auto;
+}
+.testSlide1 {
+  color: red;
+}
+.testSlide2 {
+  color: green;
+}
+.testSlide3 {
+  color: blue;
+}
+</style>
--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -11,13 +11,20 @@
           <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
       </ion-header>
-
+      <SwiperContainer />
       <ExploreContainer name="Tab 1 page" />
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from "@ionic/vue";
+import ExploreContainer from "@/components/ExploreContainer.vue";
+import SwiperContainer from "@/components/SwiperContainer.vue";
 </script>
