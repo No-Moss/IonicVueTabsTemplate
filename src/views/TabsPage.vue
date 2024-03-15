c0af5e11a4d23162add971bc18a23cf6d805bafe
Amina Mustafa <aminamustafa@Aminas-MBP.modem>
2024-03-15 12:51:30 +1100

    style: aligns desktop navigation tabs

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -5,18 +5,24 @@
       <ion-tab-bar
         ><nav>
           <ion-tab-button class="tab1" tab="tab1" href="/tabs/tab1">
-            <ion-icon aria-hidden="true" :icon="triangle" />
-            <ion-label>Tab 1</ion-label>
+            <div class="tab-button-content">
+              <ion-icon aria-hidden="true" :icon="triangle" />
+              <ion-label>Tab 1</ion-label>
+            </div>
           </ion-tab-button>
 
           <ion-tab-button tab="tab2" href="/tabs/tab2">
-            <ion-icon aria-hidden="true" :icon="ellipse" />
-            <ion-label>Tab 2</ion-label>
+            <div class="tab-button-content">
+              <ion-icon aria-hidden="true" :icon="ellipse" />
+              <ion-label>Tab 2</ion-label>
+            </div>
           </ion-tab-button>
 
           <ion-tab-button tab="tab3" href="/tabs/tab3">
-            <ion-icon aria-hidden="true" :icon="square" />
-            <ion-label>Tab 3</ion-label>
+            <div class="tab-button-content">
+              <ion-icon aria-hidden="true" :icon="square" />
+              <ion-label>Tab 3</ion-label>
+            </div>
           </ion-tab-button>
         </nav>
       </ion-tab-bar>
@@ -67,6 +73,14 @@ nav {
     flex-direction: row;
     max-height: 60px;
     width: 100%;
+  }
+  .tab-button-content {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    padding-left: 10px;
   }
 }
 </style>
